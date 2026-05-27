186b3da146101443613fc0ee3c38b471d848950e
Pravallika Navuluru <pravallikanavuluru@gmail.com>
2026-05-27 23:54:15 +0530

    add xcode project scaffold

---
+++ MeetingRobot/MeetingRobotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingRobotApp.swift
+//  MeetingRobot
+//
+//  Created by Pravallika Navuluru on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct MeetingRobotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
